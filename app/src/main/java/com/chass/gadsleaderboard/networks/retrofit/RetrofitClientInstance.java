ae21786b72db99a1ab1397c30d225e7bb6ba0d37
Obetta <francischassobetta@gmail.com>
2020-09-11 18:14:36 +0100

    Changed BASE_URL to point to the correct end point.

--- app/src/main/java/com/chass/gadsleaderboard/networks/retrofit/RetrofitClientInstance.java
+++ app/src/main/java/com/chass/gadsleaderboard/networks/retrofit/RetrofitClientInstance.java
@@ -5,8 +5,8 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClientInstance {
   private static Retrofit retrofit;
-  private static final String BASE_URL = //"https://docs.google.com/forms/d/e/";
-                                          "http://chass.me.ht/";
+  private static final String BASE_URL = "https://docs.google.com/forms/d/e/";
+                                          //"http://chass.me.ht/";
 
   public static Retrofit getRetrofitInstance() {
     if (retrofit == null) {
